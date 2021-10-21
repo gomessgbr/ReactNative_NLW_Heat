a37540390652228352c5224e7e15116dbcc30685
Gabriel Gomes <gabrielgomes98@gmail.com>
2021-10-21 17:15:28 -0300

    add header on homescreen

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { View, Text, TouchableOpacity } from 'react-native';
 
+
 import { UserPhoto }  from '../UserPhoto'
 
 import { styles } from './styles';
@@ -12,12 +13,19 @@ export function Header(){
   return (
     <View style={styles.container}>
       <LogoSvg/>
-      <UserPhoto/>
-      <TouchableOpacity>
-        <Text style={styles.logoutText}>
-          Sair
-        </Text>
-      </TouchableOpacity>
+
+      <View style={styles.logoutButton}>
+        <TouchableOpacity>
+          <Text style={styles.logoutText}>
+            Sair
+          </Text>
+        </TouchableOpacity>
+
+        <UserPhoto imageUri ='https://github.com/gomessgbr.png'/>
+      </View>
+
+      
+      
 
     </View>
   );
