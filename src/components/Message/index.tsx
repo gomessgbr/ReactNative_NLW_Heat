658e5a69a7d896a146d8cca33fb6abbac0252318
Gabriel Gomes <gabrielgomes98@gmail.com>
2021-10-21 17:57:00 -0300

    add animation on messages fromoti

--- src/components/Message/index.tsx
+++ src/components/Message/index.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import {  View, Text } from 'react-native';
 import { UserPhoto } from '../UserPhoto';
 
+import { MotiView } from 'moti';
+
 import { styles } from './styles';
 
 export type MessageProps = {
@@ -21,7 +23,12 @@ type Props = {
 
 export function Message({ data }: Props){
   return (
-    <View style={styles.container}>
+    <MotiView 
+        from={{ opacity:0, translateY: -50}}
+        animate={{ opacity:1, translateY: 0}}
+        transition={{ type: 'timing', duration: 700}}
+        style={styles.container}
+    >
         <Text style={styles.message}>
             {data.text}
         </Text>
@@ -36,6 +43,6 @@ export function Message({ data }: Props){
             </Text>
         </View>
 
-    </View>
+    </MotiView>
   );
 }
